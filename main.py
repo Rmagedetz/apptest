020de753ecd992dc1811d11bd8327b920218cf19
rmageddetz <rmagedetz@gmail.com>
2024-07-16 17:22:41 +0600

    12

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 import streamlit as st
 
-st.set_page_config(page_title="Fast Dash", page_icon=":moneybag:", layout="wide")
+# Заголовок и настройки приложения Streamlit
+st.title("Получение Telegram ID")
 
 
-def is_authenticated():
-    return "authenticated" in st.session_state and st.session_state.authenticated
+# Функция для получения Telegram ID из параметров запроса URL
+def get_telegram_id():
+    telegram_id = st.query_params.get("telegram_id")
+    if telegram_id:
+        st.success(f"Telegram ID: {telegram_id}")
+    else:
+        st.error("Telegram ID не найден в параметрах URL")
 
 
-def login():
-    with st.form("my_form"):
-        st.title("Вход в приложение")
-        password_input = st.text_input("Введите пароль", type="password")
-
-        submit = st.form_submit_button("Войти")
-        if submit:
-            if password_input == st.secrets["database"]["password"]:
-                st.success("Успешный вход!")
-                st.session_state.authenticated = True
-                show_main_page()
-            else:
-                st.error("Неверный пароль")
-
-
-def show_main_page():
-    st.title(" :moneybag: PowerDash")
-    st.markdown('<style>div.block-container{padding-top:3rem;}</style>', unsafe_allow_html=True)
-    st.write("dsf")
-
-
-if not is_authenticated():
-    login()
-else:
-    show_main_page()
+# Вызов функции
+get_telegram_id()
